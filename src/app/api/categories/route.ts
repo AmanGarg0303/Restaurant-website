ba22215d4df41c533fa8518a600970d21a147762
Aman <amangarg0303@gmail.com>
2023-08-24 22:36:42 +0530

    created featured products and get menu products route filter

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,8 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/utils/connect";
 import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
-
+// fetch all categories i.e. menu page
 export const GET = async () => {
   try {
     const categories = await prisma.category.findMany();
