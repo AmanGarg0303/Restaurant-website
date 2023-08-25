52b7bdd37fb72f6bcc40496ebfea3821641992da
Aman <amangarg0303@gmail.com>
2023-08-25 21:44:05 +0530

    cart store using zustand

--- src/types/types.ts
+++ src/types/types.ts
@@ -31,6 +31,17 @@ export type CartItemType = {
   title: string;
   img?: string;
   price: number;
-  optionTitle: string;
-  quantity: string;
+  optionTitle?: string;
+  quantity: number;
+};
+
+export type CartType = {
+  products: CartItemType[];
+  totalItems: number;
+  totalPrice: number;
+};
+
+export type ActionTypes = {
+  addToCart: (item: CartItemType) => void;
+  removeFromCart: (item: CartItemType) => void;
 };
